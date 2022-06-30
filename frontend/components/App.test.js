87a29f9a17ddb7f62301b6f1f2ca7997337cb7e8
Dougy Smith <dougysmith@Dougys-MacBook-Pro.local>
2022-06-29 20:37:11 -0600

    finished sprint 1

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,15 @@
 // Write your tests here
+import { render } from 'react-dom';
+import React from 'react';
+import AppFunctional from './AppFunctional'
+
+
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
+
+
+// test('renders without errors', async () => {
+//   render(<AppFunctional />)
+// })
